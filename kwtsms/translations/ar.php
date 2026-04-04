f9125ac770ced02721bb80d74abbf5ab5073b418
kwtSMS Admin <support@kwtsms.com>
2026-04-04 19:09:35 +0300

    feat: add English and Arabic translations

---
+++ kwtsms/translations/ar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * kwtSMS - SMS Notifications & Alerts
+ *
+ * Arabic translation file.
+ *
+ * Keys follow the PrestaShop module translation format:
+ *   <{module_name}prestashop>filename_md5(string)
+ *
+ * @author    kwtSMS <support@kwtsms.com>
+ * @copyright kwtSMS
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0
+ */
+
+global $_MODULE;
+$_MODULE = [];
+
+// kwtsms.php
+$_MODULE['<{kwtsms}prestashop>kwtsms_63e217c4ba18665166b758bcc3ec19cc'] = 'kwtSMS - إشعارات ورسائل SMS';
+$_MODULE['<{kwtsms}prestashop>kwtsms_834923792bfd26c2bd3063972dd24d1b'] = 'إرسال رسائل SMS إلى العملاء والمشرفين عبر بوابة kwtSMS. تأكيدات الطلبات، وتحديثات الحالة، وتنبيهات العملاء الجدد، وتأكيدات الدفع، وتنبيهات انخفاض المخزون.';
+$_MODULE['<{kwtsms}prestashop>kwtsms_c535d23efeb48556bcde6062de3eb9b3'] = 'هل أنت متأكد من رغبتك في إلغاء تثبيت kwtSMS؟ سيتم حذف جميع سجلات الرسائل والإعدادات.';
---
+++ kwtsms/translations/en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * kwtSMS - SMS Notifications & Alerts
+ *
+ * English translation file.
+ *
+ * Keys follow the PrestaShop module translation format:
+ *   <{module_name}prestashop>filename_md5(string)
+ *
+ * @author    kwtSMS <support@kwtsms.com>
+ * @copyright kwtSMS
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0
+ */
+
+global $_MODULE;
+$_MODULE = [];
+
+// kwtsms.php
+$_MODULE['<{kwtsms}prestashop>kwtsms_63e217c4ba18665166b758bcc3ec19cc'] = 'kwtSMS - SMS Notifications & Alerts';
+$_MODULE['<{kwtsms}prestashop>kwtsms_834923792bfd26c2bd3063972dd24d1b'] = 'Send SMS notifications to customers and admins via kwtSMS gateway. Order confirmations, status updates, new customer alerts, payment confirmations, and low stock alerts.';
+$_MODULE['<{kwtsms}prestashop>kwtsms_c535d23efeb48556bcde6062de3eb9b3'] = 'Are you sure you want to uninstall kwtSMS? All SMS logs and settings will be deleted.';
