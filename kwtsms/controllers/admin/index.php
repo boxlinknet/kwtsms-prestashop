6e2ea7d3402a10f20d7db3141c311ccbce1762ed
kwtSMS Admin <support@kwtsms.com>
2026-04-04 18:49:53 +0300

    feat: scaffold kwtsms module with composer, config, and directory structure

---
+++ kwtsms/controllers/admin/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * kwtSMS - SMS Notifications & Alerts
+ *
+ * @author    kwtSMS <support@kwtsms.com>
+ * @copyright kwtSMS
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
